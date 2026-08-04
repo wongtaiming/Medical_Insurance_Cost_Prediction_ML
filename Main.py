750a2ac1de816cdc822a064f500f155a577ce7ad
Lumina609 <82296006+Lumina609@users.noreply.github.com>
2026-08-04 13:34:15 +0800

    Change model loading to random forest model

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 # Load model
-model = joblib.load("models/multiple_linear_regression_model.pkl")
+model = joblib.load("models/random_forest_model.pkl")
 
 st.title("🏥 Medical Insurance Cost Prediction")
 
